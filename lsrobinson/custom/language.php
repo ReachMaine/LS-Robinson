1e6f1e37f3e50b186756c8419824548de60669e9
Linda "zig" Speight <linda@reachmaine.com>
2017-05-01 16:30:01 -0400

    Our team styling & language replaces

--- lsrobinson/custom/language.php
+++ lsrobinson/custom/language.php
@@ -1,25 +1,17 @@
-<?php 
-/* languages customizations 
+<?php
+/* languages customizations
 */
 	if ( !function_exists('eai_change_theme_text') ){
 		function eai_change_theme_text( $translated_text, $text, $domain ) {
 			 /* if ( is_singular() ) { */
-			    switch ( $translated_text ) {
-
-		            case 'Category' :
-		                $translated_text = __( '',  $domain  );
-		                break;
-		            /*case 'Type here...':
-		            	$translated_text = __( 'Search...',  $domain  );
-		            	break;
-		            case 'BLOG CATEGORIES':
-		            	$translated_text = __( 'Found in',  $domain  );
-		            	break;
-		            case 'Share this post:':
-		            	$translated_text = __('Share', ' $domain );
-		            	break; */
-		        }
-		    /* } */
+			    switch ($domain) {
+						case  'our-team-by-woothemes':
+								switch ( $translated_text ) {
+										case 'Email ' :
+											$translated_text = '<i class="fa fa-envelope"></i> ';
+										break;
+									} /* end our-team*/
+					} /* end switch on domain */
 
 	    	return $translated_text;
 		}
