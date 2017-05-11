854379ccabbec9a698e6afaa83b281ed1435020e
Linda "zig" Speight <linda@reachmaine.com>
2017-05-11 16:20:11 -0400

    getting next & previous to work as expected
    on single home tempate.

--- lsrobinson/single-homes.php
+++ lsrobinson/single-homes.php
@@ -78,10 +78,10 @@ if ( 'wpestate_message' == get_post_type() || 'wpestate_invoice' == get_post_typ
         <?php
         echo '<div class="homes-nextprev-wrap">';
           echo '<div class="homes-prev">';
-        previous_post_link('%link', '&lt;&lt; %title', TRUE /* same category */ );
+            next_post_link('%link', '&lt;&lt; %title', TRUE /* same category */ );
           echo '</div>'; // end-next
           echo '<div class="homes-next">';
-        next_post_link('%link', "%title &gt;&gt;",  TRUE  /*same category */ );
+            previous_post_link('%link', "%title &gt;&gt;",  TRUE  /*same category */ );
           echo '</div>'; // end-prev
         echo '</div>'; // end wrap
         ?>
