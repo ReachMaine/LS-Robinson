cf32805b2e6d8fd8333715ecd5134b8ab6b6642f
linda@reachmaine.com <linda@reachmaine.com>
2018-10-29 17:19:28 -0400

    rebuilding theme.

---
+++ lsr/custom/language.php
@@ -0,0 +1,36 @@
+<?php
+/* languages customizations
+*/
+	if ( !function_exists('eai_change_theme_text') ){
+		function eai_change_theme_text( $translated_text, $text, $domain ) {
+			 /* if ( is_singular() ) { */
+			    switch ($domain) {
+						case  'our-team-by-woothemes':
+								switch ( $translated_text ) {
+										case 'Email ' :
+											$translated_text = '<i class="fa fa-envelope"></i> ';
+										break;
+									}
+
+								break;/* end our-team*/
+						case 'wpestate':
+						switch ( $translated_text ) {
+								case 'Price per night' :
+									$translated_text = 'Price';
+								break;
+								case 'All Sizes' :
+									$translated_text = "All Area Types";
+								break;
+								case 'Where do you want to go ?':
+									$translated_text = "Which town?";
+								break;
+							}
+						break;
+					} /* end switch on domain */
+
+	    	return $translated_text;
+		}
+		add_filter( 'gettext', 'eai_change_theme_text', 20, 3 );
+	}
+
+?>
---
+++ lsr/functions.php
@@ -0,0 +1,22 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
+    function chld_thm_cfg_parent_css() {
+        wp_enqueue_style('wpestate_bootstrap',get_template_directory_uri().'/css/bootstrap.css', array(), '1.0', 'all');
+        wp_enqueue_style('wpestate_bootstrap-theme',get_template_directory_uri().'/css/bootstrap-theme.css', array(), '1.0', 'all');
+        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css' );
+        wp_enqueue_style('wpestate_media',get_template_directory_uri().'/css/my_media.css', array(), '1.0', 'all');
+    }
+endif;
+load_child_theme_textdomain('wprentals', get_stylesheet_directory().'/languages');
+add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css' );
+
+// END ENQUEUE PARENT ACTION
+
+require_once(get_stylesheet_directory().'/custom/branding.php');
+require_once(get_stylesheet_directory().'/custom/language.php');
