3c504ce769625c1015faaa880c52814a91b193a5
linda@reachmaine.com <linda@reachmaine.com>
2019-01-25 09:35:47 -0500

    remove old child theme

--- lsrobinson/custom/custom.js
+++
@@ -1,7 +0,0 @@
-jQuery(document).ready(function( $ ) {
-
-  $('#view_more_desc').trigger( "click" )
-
-  $('#listing_details').trigger( "click" )
-
-});
--- lsrobinson/custom/woocommerce.php
+++
@@ -1,7 +0,0 @@
-<?php 
-/* woocommerce customizations 
-*/
-// to remove sku from everywhere....
-add_filter( 'wc_product_sku_enabled', '__return_false' );
-
-?>
