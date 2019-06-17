ca48a1e92d337566a1c8ca56e207a81cc8201207
linda@reachmaine.com <linda@reachmaine.com>
2019-06-17 09:16:57 -0400

    update functions.php as required by theme.

--- wprentals-child-po/functions.php
+++ wprentals-child-po/functions.php
@@ -5,15 +5,24 @@ if ( !defined( 'ABSPATH' ) ) exit;
 // BEGIN ENQUEUE PARENT ACTION
 // AUTO GENERATED - Do not modify or remove comment markers above or below:
 
-if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
-    function chld_thm_cfg_parent_css() {
-        wp_enqueue_style('wpestate_bootstrap',get_template_directory_uri().'/css/bootstrap.css', array(), '1.0', 'all');
-        wp_enqueue_style('wpestate_bootstrap-theme',get_template_directory_uri().'/css/bootstrap-theme.css', array(), '1.0', 'all');
-        wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css' );
-        wp_enqueue_style('wpestate_media',get_template_directory_uri().'/css/my_media.css', array(), '1.0', 'all');
-    }
+if ( !function_exists( 'wpestate_chld_thm_cfg_parent_css' ) ):
+   function wpestate_chld_thm_cfg_parent_css() {
+
+    $parent_style = 'wpestate_style';
+    wp_enqueue_style('bootstrap',get_template_directory_uri().'/css/bootstrap.css', array(), '1.0', 'all');
+    wp_enqueue_style('bootstrap-theme',get_template_directory_uri().'/css/bootstrap-theme.css', array(), '1.0', 'all');
+    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css',array('bootstrap','bootstrap-theme'),'all' );
+    wp_enqueue_style( 'wpestate-child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array( $parent_style ),
+        wp_get_theme()->get('Version')
+    );
+
+   }
+    
 endif;
-add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css' );
+add_action( 'wp_enqueue_scripts', 'wpestate_chld_thm_cfg_parent_css' );
+load_child_theme_textdomain('wprentals', get_stylesheet_directory().'/languages');
 
 // END ENQUEUE PARENT ACTION
 require_once(get_stylesheet_directory().'/custom/branding.php');
